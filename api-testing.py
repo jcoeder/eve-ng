656aacebcbbc3df4e0200a7c815bcd5d7c19c197
Justin Oeder <jcoeder@gmail.com>
2019-05-12 00:42:40 -0400

    learning

--- api-testing.py
+++ api-testing.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import pprint
 
 # Disable SSL Warnings
 requests.urllib3.disable_warnings()
@@ -7,8 +9,22 @@ cookies = {}
 
 data = '{"username":"admin","password":"eve"}'
 
-response = requests.post('https://172.31.33.42/api/auth/login', cookies=cookies, data=data, verify=False)
+try:
+    response = requests.post('https://172.31.33.42/api/auth/login', cookies=cookies, data=data, verify=False)
+    cookies = response.cookies
+except:
+    print('An error occured here')
 
-print(response)
-print(response.text)
-print(cookies)
+headers = {'Content-type': 'application/json',}
+try:
+    response = requests.get('https://172.31.33.42/api/status', headers=headers, cookies=cookies, verify=False)
+    pprint.pprint(response.json())
+except:
+    print('An error occured there')
+
+
+try:
+    response = requests.get('https://172.31.33.42/api/list/templates/', headers=headers, cookies=cookies, verify=False)
+    pprint.pprint(response.json())
+except:
+    print('An error occured there')
